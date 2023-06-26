595deecadb9c3d377f2a41ae4ec73f982c0095fc
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-06-26 18:17:33 +0900

    Add files via upload

---
+++ js/project07.js
@@ -0,0 +1,142 @@
+$(function () {
+    //header.on
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    });
+
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+
+        //슬라이드 숫자 표시(1/3)
+        $('.main_visual .slide_num span').text(c ? (c + 1) : 1);
+        $('.main_visual .slide_num strong').text(s.slideCount);
+
+        console.log(s.slideCount);
+
+        $('.main_visual .menu li').eq(0).addClass('on');
+        $('.main_visual .menu li').eq(c).addClass('on')
+            .siblings().removeClass('on');
+    });
+
+    $('.main_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1000,
+        pauseOnHover: false,
+        fade: true,
+        arrows: false,
+    });
+
+
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+    $('.main_visual .menu li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index(); // 0,1,2
+        $('.main_slide').slick('slickGoTo', idx);
+    })
+
+
+    //tab menu
+    $('.tab_link li').on('click', function (event) {
+        event.preventDefault();
+        let idx = $(this).index(); //0,1,2
+
+        $(this).addClass('on')
+            .siblings().removeClass('on');
+
+        //방법 1
+        //$('.tab_content .con').removeClass('on');
+        // $('.tab_content .con').eq(idx).addClass('on')
+
+        //방법 2
+        $('.tab_content .con').eq(idx).addClass('on')
+            .siblings().removeClass('on');
+
+
+        console.log(event, event.target, event.currentTarget, $(this), $(this).index());
+
+
+    });
+
+
+    //메인 슬라이드 스크롤 아래 버튼
+    $('.scr').on('click', function (e) {
+        e.preventDefault();
+        const st = $(this.hash).offset().top;
+        console.log(st);
+        $('html, body').animate({ scrollTop: st - 100 }, 600)
+    });
+
+
+    $('.travel_slide').on('init afterChange', function (e, s, c) {
+        console.log(c);
+        $('.main_travel .itm').eq(c).addClass('on')
+            .siblings().removeClass('on')
+
+    })
+    $('.travel_slide').slick({
+        centerMode: true,
+        variableWidth: true,
+        arrows: false,
+        dots: true,
+    });
+
+
+
+
+
+    $('.main_travel .tab_arrows .left').on('click', function () {
+        $('.travel_slide').slick('slickPrev')
+    });
+    $('.main_travel .tab_arrows .right').on('click', function () {
+        $('.travel_slide').slick('slickNext')
+    });
+
+
+    // .main_event에 화살표 버튼 눌러서 슬라이드 돌아가게 하기
+    $('.main_event .arrows .left').on('click', function () {
+        $('.event_slide').slick('slickPrev');
+    });
+    $('.main_event .arrows .right').on('click', function () {
+        $('.event_slide').slick('slickNext');
+    });
+
+
+    $('.event_slide').slick({
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+    //to_top
+    //to_top 스크롤 위로 올리기
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 600)
+    });
+
+    // to_top 스크롤 숨겼다가 나타내기
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        // sct > 1000 ? $('.to_top').fadeIn(1000) : $('.to_top').fadeOut();
+        sct > 400 ? $('.to_top').addClass('on') : $('.to_top').removeClass('on');
+    });
+})
